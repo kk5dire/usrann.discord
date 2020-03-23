2439466395c27de257af5ceb58b6eaab64a73ae2
Damianswankkd4 <52354566+Damianswankkd4@users.noreply.github.com>
2020-03-23 18:44:41 -0400

    added delete and creat command
    cont figure ou how to send mess age in other channel (figure out later)

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = "NjkxNTU4MzYzNTA2NjcxNjM2.XnkuHA.mPbavJKC1sxObvr0wZs7W4K4AO8";
 
+const PREFIX = 'ua!';
+
 bot.on('ready', () =>{
     console.log("The Bot Is Now Currently online");
 })
 
+bot.on('message', message=>{
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+    
+     case 'ping':
+         message.channel.send('usrann.js responded with running statement');
+         break;
+
+         case 'announce':
+         if(args[1] === 'create'){
+           
+            message.channel.send(`${args[2]}` + ": " + args[3]);
+           
+         }if(args[1] === 'delete'){
+         
+                message.channel.bulkDelete(args[2]);
+            
+         }
+         break;
+
+
+
+    }
+})
 
 bot.login(token);
